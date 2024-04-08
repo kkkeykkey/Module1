b9521b6a598cb0e9d30fdf8702f8d35933a4ebcb
kkkeykkey <166177379+kkkeykkey@users.noreply.github.com>
2024-04-08 14:36:03 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Введите Ваше ФИО: ")
+print("Вот примеры написания Вашего ФИО: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print("Первая буква вашего ФИО:", my_string[0])
+print("Последняя буква Вашего ФИО:", my_string.replace(' ', '').upper()[-1])
